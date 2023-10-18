b7f4079a94f055f2ff86afeab9e26d5bd9c36844
Wonji Lee <w1024ji@gmail.com>
2023-10-18 21:31:32 +0900

    display images on post_list

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    # 미디어 파일 관리하기 위해 추가함 (아랫줄)
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
