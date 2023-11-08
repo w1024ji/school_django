59067ed66a986add0e124aa9abe4144baeb15738
Wonji Lee <w1024ji@gmail.com>
2023-11-08 15:19:08 +0900

    add author field

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 # Create your models here.
@@ -16,10 +17,12 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     # author field will be later written
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}' 
     
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
